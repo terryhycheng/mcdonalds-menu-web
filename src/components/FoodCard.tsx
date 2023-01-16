4fb05165e3e19cf3517d45bcb7918668496e1a83
terryhycheng <terryhycheng@gmail.com>
2023-01-16 10:56:44 +0000

    Added modal and its logic

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -1,18 +1,45 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { foodDataType } from "../Types/types";
+import { Modal } from "../components/Modal";
 
-export const FoodCard: FC<foodDataType> = ({ name, calories, imageUrl }) => {
+export const FoodCard: FC<foodDataType> = ({
+  id,
+  name,
+  calories,
+  imageUrl,
+  description,
+}) => {
+  const [isModal, setIsModal] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = isModal ? "hidden" : "unset";
+  }, [isModal]);
+
   return (
-    <div className="text-center shadow-md p-8 rounded-md">
-      <img
-        src={imageUrl}
-        alt=""
-        className="object-contain w-full max-h-[100px] my-3"
-      />
-      <h2 className="font-bold">{name}</h2>
-      <p className="text-sm font-light">
-        {Math.round(calories * 4.184)} kJ | {calories} kcal
-      </p>
-    </div>
+    <>
+      {isModal && (
+        <Modal
+          description={description}
+          name={name}
+          calories={calories}
+          imageUrl={imageUrl}
+          setIsModal={setIsModal}
+        />
+      )}
+      <div
+        className="text-center shadow-md p-8 rounded-md group cursor-pointer hover:border-secondary-color border border-white"
+        onClick={() => setIsModal(true)}
+      >
+        <img
+          src={imageUrl}
+          alt=""
+          className="object-contain w-full max-h-[100px] my-3"
+        />
+        <h2 className="font-bold group-hover:text-secondary-color">{name}</h2>
+        <p className="text-sm font-light">
+          {Math.round(calories * 4.184)} kJ | {calories} kcal
+        </p>
+      </div>
+    </>
   );
 };
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,40 @@
-export const Modal = () => {
-  return <div>Modal</div>;
+import { FC } from "react";
+
+type PropsType = {
+  description: string;
+  name: string;
+  imageUrl: string;
+  calories: number;
+  setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const Modal: FC<PropsType> = ({
+  name,
+  description,
+  imageUrl,
+  calories,
+  setIsModal,
+}) => {
+  return (
+    <div className="fixed flex top-0 left-0 bg-black w-full h-[100vh] bg-opacity-50 justify-center">
+      <div className="grid md:grid-cols-2 p-10 gap-4 justify-center items-center bg-white max-w-[800px] m-auto mx-2 shadow-sm rounded-md">
+        <img src={imageUrl} alt="food-image" />
+        <div className="flex flex-col gap-4 justify-center items-center">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <h1 className="font-bold text-2xl">{name}</h1>
+            <p className="text-sm font-light">
+              {Math.round(calories * 4.184)} kJ | {calories} kcal
+            </p>
+          </div>
+          <p>{description}</p>
+          <button
+            className="bg-secondary-color p-3 w-full md:max-w-[350px] max-h-[50px] mx-auto font-light mt-4 rounded-md"
+            onClick={() => setIsModal(false)}
+          >
+            close
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
